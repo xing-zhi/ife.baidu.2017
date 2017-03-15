e04c863d0919ba32bede554184d5352959176006
xing-zhi <xing_zhi@126.com>
2017-03-15 09:50:48 +0800

    college 8: task 2

---
+++ college.8/task-02/js/app.js
@@ -0,0 +1,19 @@
+function Observer(obj) {
+  const data = {};
+  const innerData = {};
+
+  Object.keys(obj).forEach(key => {
+    Object.defineProperty(data, key, {
+      get() {
+        console.log(`你访问了${key}`);
+        return innerData[key];
+      },
+      set(value) {
+        console.log(`你设置了${key}，新值为${value}`);
+        innerData[key] = value;
+      }
+    });
+  });
+
+  return { data };
+}
