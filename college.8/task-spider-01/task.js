fa5eb2f9b60342f803e5fe6d279d1b54c23c2bf4
xing-zhi <xing_zhi@126.com>
2017-03-21 19:04:54 +0800

    basic spider

---
+++ college.8/task-spider-01/task.js
@@ -0,0 +1,78 @@
+var webPage = require('webpage');
+var system = require('system');
+
+var page = webPage.create();
+
+if ( system.args.length === 1 ) {
+  console.log('Usage: task.js <keyword>');
+  phantom.exit();
+}
+
+//在PhantomJS中 执行JS函数的时候是不能直接使用Phantom的方法的。需要hook住alert方法，再使用alert来输出
+page.onAlert = function(msg) {
+  console.log('Alert', msg);
+};
+page.onError = function(msg) {
+  console.log('Error', msg);
+};
+
+var keyword = system.args.slice(1).join(' ');
+var startTime = Date.now();
+
+page.injectJs('http://apps.bdimg.com/libs/jquery/1.6.4/jquery.js');
+page.open("https://www.baidu.com", function(status) {
+  if ( status !== 'success' ) {
+    var now = Date.now();
+    var result = {
+      code: 0,
+      msg: '抓取失败',
+      word: keyword,
+      time: now - startTime,
+      dataList: []
+    };
+
+    console.log(JSON.stringify(result));
+    phantom.exit();
+  }
+
+  page.evaluate(function(text) {
+    $("#kw").val(text);
+    $("#su").click();
+  }, keyword);
+
+  setTimeout(function() {
+    var results = page.evaluate(function() {
+      const results = document.querySelectorAll('.c-container');
+
+      return [].filter.call(results, function(result) {
+        return !result.getAttribute('mu');
+      }).map(function(result) {
+        var title = result.querySelector('h3.t a').textContent;
+        var info = result.querySelector('.c-abstract').textContent;
+        var link = result.querySelector('.c-showurl');
+        var pic = result.querySelector('.c-img');
+        var data = {
+          title: title,
+          info: info,
+          link: link.nodeName.toLowerCase() === 'a' ? link.href : link.textContent
+        };
+
+        data.pic = pic ? pic.src : '';
+
+        return data;
+      });
+    });
+    var now = Date.now();
+    var result = {
+      code: 1,
+      msg: '抓取成功',
+      word: keyword,
+      time: now - startTime,
+      dataList: results
+    };
+
+    console.log(JSON.stringify(result));
+    phantom.exit();
+  }, 2000);
+
+});
